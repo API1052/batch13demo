c849c19ce84ca270252a6a7fb39da18fe9ae02f7
API1052 <81964466+API1052@users.noreply.github.com>
2021-04-05 17:09:19 +0530

    Iam second commit

--- Demo.java
+++ Demo.java
@@ -2,6 +2,8 @@ class Demo{
 	public static void main(String[] args){
 	System.out.println("Welcome");
 	System.out.println("Hello");
+	System.out.println("Yo ");
+	System.out.println("Namastay");
 }
 
 }
